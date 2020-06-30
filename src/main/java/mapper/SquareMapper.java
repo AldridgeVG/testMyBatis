5a072fd33475bc6703d37769bd4b48dbbb32bc10
aldridgevg <caxuser@live.com>
2020-06-30 10:56:33 +0800

    adjust readme

--- src/main/java/mapper/SquareMapper.java
+++ src/main/java/mapper/SquareMapper.java
@@ -2,5 +2,7 @@ package mapper;
 import pojo.Square;
 
 public interface SquareMapper {
+
+    // this method won't be realized, mapper only get the parameter of this method as param of SQL
     public Square getSquare(int length);
 }
